bd72b75142d12380f48f10539fdd29b36036d99f
Buchner Johannes <e0625457@student.tuwien.ac.at>
2009-04-25 03:24:07 +0200

    added matrix and sum tool

---
+++ tools/matrix_man.c
@@ -0,0 +1,435 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_vector.h>
+
+#include <string.h>
+#include <ctype.h>
+
+#define SEP '\t'
+#define OUTPUTFORMAT "%e\t"
+
+#ifdef DEBUG
+#define IFDEBUG if(1)
+#else
+#define IFDEBUG if(0)
+#endif
+
+void usage(char * progname) {
+	printf("%s: SYNOPSIS: <command>\n"
+		"\n"
+		"matrix_man allows manipulation of double values tables\n"
+		"It reads csv or tab-seperated values (double) from stdin,\n"
+		"performs the commands and prints the result to stdout.\n"
+		"\ncurrently supported commands\n", progname);
+	printf(""
+		"\trm <i>           remove column i\n"
+		"\tnew <i>          add a new column at i and fill it with zeroes\n"
+		"\tswap <i> <o>  swap the value in column i+o with i\n"
+		"\tinvm <i>         set i to i^(-1)\n"
+		"\tinva <i>         set i to -i\n"
+		"\tdiff <i>         set i to the difference to the previous line\n");
+	printf(""
+		"\tadd <i> <j>      add the value in column j to i. \n"
+		"\t                 i and j can only specify one column each\n"
+		"\tadd_rel <i> <o>  add the value in column i+o to i\n"
+		"\taddc <i> <v>     add the value v to i\n"
+		"\tmul <i> <j>      multiply the value in column j onto the value in i\n"
+		"\t                 i and j can only specify one column each\n"
+		"\tmulc <i> <v>     multiply i with the value v\n"
+		"\tmul_rel <i> <o>  multiply the value in column i+o onto i\n");
+	printf("\n"
+		"\tfor any column number a column selection can be given:"
+		"\t\ti%%2=0,i>3,i<2,i=2,i/4\n"
+		"\t, acts as a boolean 'and'. / means unequal. \n"
+		"\tabove, o is always a offset number\n"
+		"\n");
+	exit(1);
+
+}
+
+/**
+ * @returns -1 on error
+ */
+int qualifies_parse(int i, char * qualification_in) {
+	int j = 0;
+	int k = 0;
+	int last;
+	int len = strlen(qualification_in);
+	char * qualification;
+	int tokens[5] = { -1, -1, -1, -1, -1 };
+	int result = -1;
+
+	qualification = (char*) calloc(len + 1, sizeof(char));
+	if(qualification == NULL) {
+		perror("could not allocate buffer");
+		exit(-1);
+	}
+	strcpy(qualification, qualification_in);
+
+	while (qualification[k] != ',' && k < len) {
+		if (qualification[k] == '%' || qualification[k] == '<'
+				|| qualification[k] == '>' || qualification[k] == '='
+				|| qualification[k] == '/') {
+			tokens[j] = k;
+			j++;
+		}
+		k++;
+	}
+	if (k == len)
+		last = 1;
+	else {
+		last = 0;
+		qualification[k] = 0;
+	}
+	IFDEBUG
+		printf("DEBUG: qual: %s, i: %d\n", qualification, i);
+	IFDEBUG
+		printf("DEBUG: qual: len: %d, mylen: %d, ntokens: %d\n", len, k, j);
+	if (j == 1 && qualification[0] == 'i') {
+		IFDEBUG
+			printf("DEBUG: qual: relation\n");
+		if (qualification[tokens[0]] == '<') {
+			if (atoi(&qualification[tokens[0] + 1]) < i) {
+				result = 1;
+			} else {
+				result = 0;
+			}
+		} else if (qualification[tokens[0]] == '>') {
+			if (i > atoi(&qualification[tokens[0] + 1])) {
+				result = 1;
+			} else {
+				result = 0;
+			}
+		} else if (qualification[tokens[0]] == '=') {
+			if (i == atoi(&qualification[tokens[0] + 1])) {
+				result = 1;
+			} else {
+				result = 0;
+			}
+		} else if (qualification[tokens[0]] == '/') {
+			if (i != atoi(&qualification[tokens[0] + 1])) {
+				result = 1;
+			} else {
+				result = 0;
+			}
+		}
+	} else if (j == 2 && qualification[0] == 'i' && qualification[tokens[0]]
+			== '%' && qualification[tokens[1]] == '=') {
+		IFDEBUG
+			printf("DEBUG: qual: modulo\n");
+		qualification[tokens[0]] = 0;
+		qualification[tokens[1]] = 0;
+		if ((i + 1) % atoi(&qualification[tokens[0] + 1]) == atoi(
+				&qualification[tokens[1] + 1])) {
+			result = 1;
+		} else {
+			result = 0;
+		}
+	}
+	free(qualification);
+	IFDEBUG
+		printf("DEBUG: qual: result: %d\n", result);
+	if (result == -1)
+		return result;
+	if (result == 0 || last == 1) {
+		return result;
+	} else {
+		return qualifies_parse(i, &qualification_in[k + 1]);
+	}
+}
+
+int qualifies(int i, char * qualification) {
+	int result = qualifies_parse(i, qualification);
+	if (result == -1) {
+		fprintf(stderr, "error on qualification string\n");
+		exit(2);
+	} else {
+		return result;
+	}
+}
+
+void command_rm(int count, gsl_vector * current, char * qualification) {
+	int i;
+	for (i = 0; i < count; i++) {
+		if (!qualifies(i, qualification))
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i));
+	}
+	printf("\n");
+	fflush(stdout);
+}
+void command_inva(int count, gsl_vector * current, char * qualification) {
+	int i;
+	for (i = 0; i < count; i++) {
+		if (qualifies(i, qualification))
+			printf(OUTPUTFORMAT, -gsl_vector_get(current, i));
+		else
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i));
+	}
+	printf("\n");
+	fflush(stdout);
+}
+void command_invm(int count, gsl_vector * current, char * qualification) {
+	int i;
+	for (i = 0; i < count; i++) {
+		if (qualifies(i, qualification))
+			printf(OUTPUTFORMAT, 1.0 / gsl_vector_get(current, i));
+		else
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i));
+	}
+	printf("\n");
+	fflush(stdout);
+}
+void command_diff(int count, gsl_vector * prev, gsl_vector * current,
+		char * qualification) {
+	int i;
+	for (i = 0; i < count; i++) {
+		if (qualifies(i, qualification))
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i) - gsl_vector_get(
+					prev, i));
+		else
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i));
+	}
+	printf("\n");
+	fflush(stdout);
+}
+
+void command_new(int count, gsl_vector * current, char * qualification) {
+	int offset = 0;
+	int i;
+	for (i = 0; i < count; i++) {
+		if (qualifies(i + offset, qualification)) {
+			printf(OUTPUTFORMAT, 0.0);
+			offset++;
+			i--;
+		} else {
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i));
+		}
+	}
+	if (qualifies(i, qualification))
+		printf(OUTPUTFORMAT, 0.0);
+
+	printf("\n");
+	fflush(stdout);
+}
+void command_add(int count, gsl_vector * current, char * qualification_source,
+		char * qualification_target) {
+	int source = -1;
+	int target = -1;
+	int i;
+	for (i = 0; i < count; i++) {
+		if (qualifies(i, qualification_source) && source == -1) {
+			source = i;
+		}
+		if (qualifies(i, qualification_target) && target == -1) {
+			target = i;
+		}
+	}
+	if(source == -1) {
+		fprintf(stderr, "no source match\n");
+		exit(-1);
+	}
+	if(target == -1) {
+		fprintf(stderr, "no target match\n");
+		exit(-1);
+	}
+	for (i = 0; i < count; i++) {
+		if (i == target)
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i) + gsl_vector_get(
+					current, source));
+		else
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i));
+	}
+	printf("\n");
+	fflush(stdout);
+}
+void command_add_rel(int count, gsl_vector * current,
+		char * qualification_source, char * offset_str) {
+	int i;
+	int offset = atoi(offset_str);
+	for (i = 0; i < count; i++) {
+		if (qualifies(i, qualification_source)) {
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i) + gsl_vector_get(
+					current, i + offset));
+		} else {
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i));
+		}
+	}
+	printf("\n");
+	fflush(stdout);
+}
+
+void command_addc(int count, gsl_vector * current, char * qualification_source,
+		char * value_str) {
+	int i;
+	double value;
+	sscanf(value_str, "%lf", &value);
+	for (i = 0; i < count; i++) {
+		if (qualifies(i, qualification_source)) {
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i) + value);
+		} else {
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i));
+		}
+	}
+	printf("\n");
+	fflush(stdout);
+}
+void command_mul(int count, gsl_vector * current, char * qualification_source,
+		char * qualification_target) {
+	int source = -1;
+	int target = -1;
+	int i;
+	for (i = 0; i < count; i++) {
+		if (qualifies(i, qualification_source) && source == -1) {
+			source = i;
+		}
+		if (qualifies(i, qualification_target) && target == -1) {
+			target = i;
+		}
+	}
+	if(source == -1) {
+		fprintf(stderr, "no source match\n");
+		exit(-1);
+	}
+	if(target == -1) {
+		fprintf(stderr, "no target match\n");
+		exit(-1);
+	}
+	for (i = 0; i < count; i++) {
+		if (i == target)
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i) * gsl_vector_get(
+					current, source));
+		else
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i));
+	}
+	printf("\n");
+	fflush(stdout);
+}
+void command_mul_rel(int count, gsl_vector * current,
+		char * qualification_source, char * offset_str) {
+	int i;
+	int offset = atoi(offset_str);
+	for (i = 0; i < count; i++) {
+		if (qualifies(i, qualification_source)) {
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i) * gsl_vector_get(
+					current, i + offset));
+		} else {
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i));
+		}
+	}
+	printf("\n");
+	fflush(stdout);
+}
+void command_mulc(int count, gsl_vector * current, char * qualification_source,
+		char * value_str) {
+	int i;
+	double value;
+	sscanf(value_str, "%lf", &value);
+	for (i = 0; i < count; i++) {
+		if (qualifies(i, qualification_source)) {
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i) * value);
+		} else {
+			printf(OUTPUTFORMAT, gsl_vector_get(current, i));
+		}
+	}
+	printf("\n");
+	fflush(stdout);
+}
+
+void row_man(int argc, char ** argv) {
+	gsl_vector * prev = NULL;
+	gsl_vector * current = NULL;
+	FILE * input = stdin;
+	char buf[10000];
+	int count;
+	int i;
+	int col;
+	int at_whitespace;
+	int line = 0;
+	char * command = argv[1];
+	double v;
+
+	while (!feof(input)) {
+		line++;
+		current = gsl_vector_alloc(100);
+		fgets(buf, 10000, input);
+		count = 0;
+		i = 0;
+		at_whitespace = 1;
+		while (buf[i] != 0) {
+			if (isspace(buf[i])) {
+				if (at_whitespace == 0)
+					count++;
+				at_whitespace = 1;
+			} else {
+				if (at_whitespace == 1) {
+					col = sscanf(&buf[i], "%lf", &v);
+					IFDEBUG
+						printf("field %d: %f\n", count, v);
+					gsl_vector_set(current, count, v);
+
+					if (col != 1) {
+						fprintf(stderr,
+								"field could not be read: %d, line %d\n", i
+										+ count, line);
+						exit(1);
+					}
+				}
+				at_whitespace = 0;
+			}
+			i++;
+		}
+		if (at_whitespace == 0)
+			count++;
+
+		IFDEBUG
+			printf("command: %s; nargs = %d\n", command, argc);
+
+		if (0 == strcmp(command, "rm") && argc == 3)
+			command_rm(count, current, argv[2]);
+		else if (0 == strcmp(command, "new") && argc == 3)
+			command_new(count, current, argv[2]);
+		else if (0 == strcmp(command, "add") && argc == 4)
+			command_add(count, current, argv[2], argv[3]);
+		else if (0 == strcmp(command, "mul") && argc == 4)
+			command_mul(count, current, argv[2], argv[3]);
+		else if (0 == strcmp(command, "add_rel") && argc == 4)
+			command_add_rel(count, current, argv[2], argv[3]);
+		else if (0 == strcmp(command, "mul_rel") && argc == 4)
+			command_mul_rel(count, current, argv[2], argv[3]);
+		else if (0 == strcmp(command, "addc") && argc == 4)
+			command_addc(count, current, argv[2], argv[3]);
+		else if (0 == strcmp(command, "mulc") && argc == 4)
+			command_mulc(count, current, argv[2], argv[3]);
+		else if (0 == strcmp(command, "invm") && argc == 3)
+			command_invm(count, current, argv[2]);
+		else if (0 == strcmp(command, "inva") && argc == 3)
+			command_inva(count, current, argv[2]);
+		else if (0 == strcmp(command, "diff") && argc == 3) {
+			if (prev != NULL) {
+				command_diff(count, prev, current, argv[2]);
+				gsl_vector_free(prev);
+			}
+			prev = current;
+			current = NULL;
+		} else {
+			fprintf(stderr, "Unknown command\n");
+			usage(argv[0]);
+		}
+		if (current != NULL)
+			gsl_vector_free(current);
+	}
+
+}
+
+int main(int argc, char ** argv) {
+	if (argc < 3) {
+		fprintf(stderr, "Wrong number of arguments\n");
+		usage(argv[0]);
+	} else {
+		row_man(argc, argv);
+	}
+
+	return 0;
+}
---
+++ tools/sum_tool.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_histogram.h>
+#include <gsl/gsl_vector.h>
+
+#include <assert.h>
+#include <string.h>
+#include <ctype.h>
+#include "gsl_helper.h"
+#include "debug.h"
+#include "histogram.c"
+
+void usage(char * progname) {
+	fprintf(stderr, "%s: SYNOPSIS: [-s] [-1] file1 file2 ...\n"
+		"\n"
+		"\ts\tuse a square sum\n"
+		"\t1\tat the end, sum all columns up to one value\n"
+		"\n"
+		"This program sums up the values of the given files. They should contain \n"
+		"float values in one or more columns\n"
+		"\n", progname);
+}
+
+void sum_up(gsl_vector * sum, unsigned int n, const char * filename, int square) {
+	FILE * input;
+	unsigned int i;
+	int col;
+	double v;
+	int line = 0;
+
+	input = fopen(filename, "r");
+	if (input == NULL) {
+		fprintf(stderr, "error opening file %s\n", filename);
+		perror("file could not be opened");
+		exit(1);
+	}
+	while (!feof(input)) {
+		for (i = 0; i < n; i++) {
+			col = fscanf(input, "%lf", &v);
+			if (col != 1) {
+				if (feof(input))
+					break;
+				fprintf(stderr, "field could not be read: %d, line %d in %s\n",
+						i + 1, line + 1, filename);
+				exit(1);
+			}
+			if (square)
+				gsl_vector_add_constant(sum, v * v);
+			else
+				gsl_vector_add_constant(sum, v);
+		}
+		line++;
+	}
+	dump_i("read lines", line);
+	fclose(input);
+}
+
+void run(char ** filenames, unsigned int filecount, int square, int one_value) {
+	unsigned int i;
+	gsl_vector * sum;
+	unsigned int ncolumns;
+
+	debug("looking for number of columns ...");
+	ncolumns = get_column_count(filenames[0]);
+	dump_i("number of columns", ncolumns);
+	sum = gsl_vector_alloc(ncolumns);
+	gsl_vector_set_zero(sum);
+
+	debug("filling histograms ... ");
+	for (i = 0; i < filecount; i++) {
+		dump_s("with file", filenames[i]);
+		sum_up(sum, ncolumns, filenames[i], square);
+	}
+
+	if (one_value) {
+		if (square)
+			printf("%e", calc_vector_squaresum(sum));
+		else
+			printf("%e", calc_vector_sum(sum));
+	} else {
+		for (i = 0; i < ncolumns - 1; i++) {
+			printf("%e\t", gsl_vector_get(sum, i));
+		}
+		printf("%e", gsl_vector_get(sum, i));
+	}
+	printf("\n");
+	gsl_vector_free(sum);
+}
+
+int main(int argc, char ** argv) {
+	int square = 0;
+	int onevalue = 0;
+	if (argc <= 1) {
+		usage(argv[0]);
+	} else {
+		argv++;
+		argc--;
+
+		if (argc > 1 && strcmp(argv[0], "-s") == 0) {
+			square = 1;
+			argv++;
+			argc--;
+		}
+		if (argc > 1 && strcmp(argv[0], "-1") == 0) {
+			onevalue = 1;
+			argv++;
+			argc--;
+		}
+		run(argv, argc, square, onevalue);
+	}
+	return 0;
+}
