b431433fcf1750f699a0a4a592727cb235f28900
Buchner Johannes <e0625457@student.tuwien.ac.at>
2009-04-28 19:28:42 +0200

    evaluation by really summing up misses

--- script/evaluate.sh
+++ script/evaluate.sh
@@ -6,6 +6,8 @@ function die() {
 
 TMP=$(mktemp)
 mcmcdir=~/Desktop/Arbeit
+matrix_man=$mcmcdir/matrix_man.exe
+
 
 bash $mcmcdir/script/join.sh *-chain*.prob.dump || die 'join' 1
 #paste *.all > data || exit 2
@@ -15,13 +17,15 @@ done
 {
 echo counting misses 1>&2
 for j in Amplitude Phase Frequenz; do
-	cut -f1 $j.all.hist.tab |grep -v $(cut -f1 $mcmcdir/idl/simplesin/correct-$j.all.dat) > $TMP
-	$mcmcdir/sum_tool.exe $TMP
+#	cut -f1 $j.all.hist.tab |grep -v $(cut -f1 $mcmcdir/idl/simplesin/correct-$j.all.dat) > $TMP
+	cat $j.all|$matrix_man addc i=1 $(cut -f1 $mcmcdir/idl/simplesin/correct-$j.all.dat) > $TMP
+
+	$mcmcdir/sum_tool.exe $TMP|$matrix_man new i=2|$matrix_man addc i=2 $(cat $j.all|wc -l)|$matrix_man invm i=2|$matrix_man mul_rel i=1 1|$matrix_man rm i=2
 done
 
 echo stuck chains 1>&2
 chains=$(cat acceptance_rate.dump |~/Desktop/Arbeit/matrix_man.exe diff 'i>1'|grep -w '0.000000e+00' -c)
-echo $(($chains*10000)) # stuck chains are expensive
+echo $(($chains*1000)) # stuck chains are expensive
 
 } | tee $TMP 1>&2
 
