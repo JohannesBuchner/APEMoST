4cd1cedd4fbf2e6322f47988cb3c7cb81f12e321
Buchner Johannes <e0625457@student.tuwien.ac.at>
2009-05-12 13:02:47 +0200

    only looking at chain0 for evaluation/plot

--- script/plot.sh
+++ script/plot.sh
@@ -27,11 +27,18 @@ for dir in *_*; do
 	cd $dir || continue
 	echo $dir
 	bash $mcmcdir/script/join.sh *.prob.dump || die 'join' 1
+	for i in *chain0*.prob.dump 
+	do
+		target=$(echo $i|sed 's/-chain0.prob.dump//g')
+		cat $i > $target.all
+	done
 	#paste *.all > data || exit 2
 	for j in *.all; do 
 		$mcmcdir/histogram_tool.exe 500 $j | sed 's/\(.*\)\.\..*:[^0-9]*\([0-9]*\)/\1\t\2/g'|grep '\.' > $j.hist.tab || die 'histogram' 3
 		rm $j
-		echo 'set title "'$dir-$j'";set terminal png; set output "'$j.hist.tab.png'"; set term png size 400, 200; ' plot "$(getlimits $j) "'"'$j.hist.tab'" notitle with boxes, "'$mcmcdir/idl/simplesin/correct-$j.dat'" notitle linecolor 3 with points;'|gnuplot || die 'plot' 4
+		echo 'set title "'$dir-$j'";set terminal png; set output "'$j.hist.tab.png'"; set term png size 400, 200; ' plot "$(getlimits $j) "'"'$j.hist.tab'" notitle with boxes;'|gnuplot || die 'plot' 4
+		#echo 'set title "'$dir-$j'";set terminal png; set output "'$j.hist.tab.png'"; set term png size 400, 200; ' plot "$(getlimits $j) "'"'$j.hist.tab'" notitle with boxes, "'$mcmcdir/idl/simplesin/correct-$j.dat'" notitle linecolor 3 with points;'|gnuplot || die 'plot' 4
+
 		rm $j.hist.tab
 		pngtopnm $j.hist.tab.png > ../${dir}_${j}.hist.tab.pnm || die 'converting image' 5
 		rm $j.hist.tab.png
