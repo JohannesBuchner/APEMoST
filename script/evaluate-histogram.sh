41a22cf8e10392abd40b5f0ba70a9c4de5f4700b
Buchner Johannes <e0625457@student.tuwien.ac.at>
2009-05-24 06:02:35 +0200

    script for useful condensed final value output (real values)

--- script/evaluate-histogram.sh
+++ script/evaluate-histogram.sh
@@ -6,31 +6,18 @@ function die() {
 
 mcmcdir=~/Desktop/Arbeit
 measure=$mcmcdir/peaks.exe
-#measure=$mcmcdir/measure_histogram.exe
 if [ "$CATCOMMAND" == "" ]; then
 	cat="cat"
 else
 	cat="$CATCOMMAND"
 fi
 
-for dir in $*
-do
-	echo $dir
-	TMP=$(mktemp)
-	TMP2=$(mktemp)
-	for par in Amplitude Frequenz Phase
-	do 
-		file=$TMP2
-		$cat $dir/$par*chain0*.dump > $file
-		min=$(grep $par $mcmcdir/idl/simplesin/params|cut -f2)
-		max=$(grep $par $mcmcdir/idl/simplesin/params|cut -f3)
-		real=$(cut -f1  $mcmcdir/idl/simplesin/correct-$par*)
-		#echo -n $par"       "
-		$measure $min $max $file|head -n2|tail -n1|
-			awk '{print (($1-'$real')/('$max' - '$min'))^2*100000"\t"($2+$3)/('$max' - '$min') * 1000"\t"(1-$4)^4*10000;}'|tee -a $TMP
-	done
-	$mcmcdir/sum_tool.exe -1 $TMP
-	rm $TMP
-	rm $TMP2
+TMP=$(mktemp)
+cat params|cut -f4|while read param; do 
+	echo -n $param "     "; 
+	min=$(grep -w $param params|cut -f2); 
+	max=$(grep -w $param params|cut -f3); 
+	$cat $dir/$param-chain0.prob.dump > $TMP
+	$mcmcdir/peaks.exe $min $max $TMP|head -n2|tail -n1
 done
 
