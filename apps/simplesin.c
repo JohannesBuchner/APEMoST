30fb60f7b6471907e19ad7c1d207b8dffe24deb5
Johannes Buchner <buchner.johannes@gmx.at>
2009-09-16 11:46:41 +1200

    nicer variable names

--- apps/simplesin.c
+++ apps/simplesin.c
@@ -9,18 +9,18 @@
 #define SIGMA 0.5
 #endif
 
-double apply_formula(mcmc * m, unsigned int i, double param0, double param1,
-		double param2) {
+double apply_formula(mcmc * m, unsigned int i, double amplitude, double frequency,
+		double phase) {
 	double x = gsl_matrix_get(m->data, i, 0);
-	double y = param0 * gsl_sf_sin(2.0 * M_PI * param1 * x + param2);
+	double y = amplitude * gsl_sf_sin(2.0 * M_PI * frequency * x + phase);
 	return y;
 }
 
 void calc_model(mcmc * m, const gsl_vector * old_values) {
 	unsigned int i;
-	double param0 = gsl_vector_get(m->params, 0);
-	double param1 = gsl_vector_get(m->params, 1);
-	double param2 = gsl_vector_get(m->params, 2);
+	double amplitude = gsl_vector_get(m->params, 0);
+	double frequency = gsl_vector_get(m->params, 1);
+	double phase     = gsl_vector_get(m->params, 2);
 	double y;
 	double deltay;
 	double square_sum = 0;
@@ -29,7 +29,7 @@ void calc_model(mcmc * m, const gsl_vector * old_values) {
 	/*dump_v("recalculating model for parameter values", m->params);*/
 	for (i = 0; i < m->data->size1; i++) {
 		y = gsl_matrix_get(m->data, i, 1);
-		deltay = apply_formula(m, i, param0, param1, param2) - y;
+		deltay = apply_formula(m, i, amplitude, frequency, phase) - y;
 		square_sum += deltay * deltay;
 	}
 	set_prob(m, get_beta(m) * square_sum / (-2 * SIGMA * SIGMA));
